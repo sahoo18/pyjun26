855cd96c7f40080b7fe970737f9cc534ace333f5
Bidyadhar Sahoo <bidyadharsahoo528@gmail.com>
2026-04-10 08:42:03 +0530

    Added Day 3 assignments

---
+++ Month-1/Week-1/Day-1/Practice/index.py
@@ -0,0 +1,42 @@
+# school="ABC School"
+# age=23
+# address="123 Main Street"
+# building_name="sky Tower"
+
+# Data type
+# primitive data type
+
+# name="Rahul"
+# age=19
+# cgpa=9.2
+# is_valid = True
+
+# print (type(age))
+
+# name = input("Enter your full name :")
+# age =input("Enter your age:")
+# age_in_number
+
+
+# Arithmatic operator
+
+x= int(input("Enter the value of x :"))
+y= int(input("Enter the value of y :"))
+
+# addition
+print("The sum of x and y is :",x+y)
+
+
+# Logical operator
+
+print(True or False)
+
+# Comparison operator
+
+age= input("Enter your age : ")
+
+print(age == 5)
+
+
+
+
