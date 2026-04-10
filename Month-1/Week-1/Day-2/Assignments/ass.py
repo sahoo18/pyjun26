b133b009616069e016e42570e83de683c8dd9992
Bidyadhar Sahoo <bidyadharsahoo528@gmail.com>
2026-04-10 08:38:27 +0530

    Add Day 2 assignments

---
+++ Month-1/Week-1/Day-2/Assignments/ass.py
@@ -0,0 +1,43 @@
+#// Data types
+age = 20
+height = 5.8
+name = "Bidyadhar"
+is_student = True
+
+print(age)
+print(height)
+print(name)
+print(is_student)
+
+
+
+#// User Input
+name = input("Enter your name: ")
+
+print("Hello,", name)
+
+
+
+
+#// Operators
+#// 1.Arithmatic operators
+a = 10
+b = 5
+
+print("Addition =", a + b)
+print("Subtraction =", a - b)
+print("Multiplication =", a * b)
+print("Division =", a / b)
+
+#// 2.Relational operators
+a = 10
+b = 20
+
+print(a < b)
+print(a == b)
+
+#// 3. Logical operators
+a = 10
+b = 20
+
+print(a < b and b > 15)
